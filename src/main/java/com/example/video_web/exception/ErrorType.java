38cc36c85a6e3ad678c707b4ec0bda7ca860ba51
2yue <yue_1227@outlook.com>
2022-05-19 03:48:32 +0800

    import: 统一返回类型

---
+++ src/main/java/com/example/video_web/exception/ErrorType.java
@@ -0,0 +1,17 @@
+package com.example.video_web.exception;
+
+public interface ErrorType {
+    /**
+     * 返回code
+     *
+     * @return
+     */
+    String getCode();
+
+    /**
+     * 返回mesg
+     *
+     * @return
+     */
+    String getMessage();
+}
